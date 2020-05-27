796da290433d840e39834ec05eed2e0f30916b05
fengwenjun <kael@fengwenjun.com>
2020-05-27 21:50:12 +0800

    signature ok

--- app/controllers/api/v1/public_controller.rb
+++ app/controllers/api/v1/public_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::PublicController < Api::V1::ApplicationController
   def signature
     Rails.logger.info("params: #{params}")
-    a = [params[:signature].to_s, params[:timestamp].to_s, params[:nonce].to_s]
+    token = Rails.application.credentials.metrix_token
+    a = [token, params[:timestamp].to_s, params[:nonce].to_s]
     b = Digest::SHA1.hexdigest(a.sort.join('')) == params[:signature]
     r = b ? params[:echostr] : 'false'
     render plain: r
