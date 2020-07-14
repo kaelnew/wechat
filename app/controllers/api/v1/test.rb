6536663c2de1176054894a0adc479979724b69a2
fengwenjun <kael@fengwenjun.com>
2020-07-14 16:29:26 +0800

    rm test.rb

--- app/controllers/api/v1/test.rb
+++
@@ -1,8 +0,0 @@
-lines = IO.readlines('debug.log')
-num_of_ip = {}
-lines.each do |line|
-  ip = line.match(/(?<="client_ip":")([\d|\.]+)/).to_a
-  next if ip[0].nil?
-  num_of_ip[ip[0]] ||= 0
-  num_of_ip[ip[0]] += 1
-end
